c13cc68891c87bed676b4e7bb8fe422429fce75d
yisehak <yisehak.aw@gmail.com>
2019-08-08 15:41:26 +0300

    Add routes, Insert background image

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 import AnnotationForm from "./pages/form";
+import AnnotationResult from "./pages/result";
+import PageNotFound from "./pages/page-not-found";
 
+import bg from "./assets/bg.svg";
 import "antd/dist/antd.min.css";
 import "./style.css";
 
 function App(props) {
   return (
-    <Router>
-      <Route path="/" exact component={AnnotationForm} />
-    </Router>
+    <div style={{ height: "100vh", backgroundImage: `url(${bg})` }}>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={AnnotationForm} />
+          <Route path="/result/:id" component={AnnotationResult} />
+          <Route component={PageNotFound} />
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
